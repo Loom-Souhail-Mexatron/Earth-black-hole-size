19b3057a064faec43f56f4effc3f5e7702c5b052
Loom-Souhail-Mexatron <54601024+Loom-Souhail-Mexatron@users.noreply.github.com>
2021-09-10 16:44:46 +0100

    Added masses of other planets
    Now instead of just the Earth, there is the sun and the planets in the solar system.

--- Earth black hole.py
+++ Earth black hole.py
@@ -1,8 +1,21 @@
 from math import * #Import the math library to use pi
-mass = 5.962 * pow(10, 24) #Kg
+#mass = 5.962 * pow(10, 24) in Kg, the mass of Earth
 gravity = 6.67430 * pow(10, -11) #m^3.Kg^-1.s^-1
 c = 299792458 #m/s
 
+#Masses of different planets in our solar system and the sun
+masses = {
+	"Sun" 	  : 1.989 * pow(10, 30),
+	"Mercury" : 3.285 * pow(10, 23),
+	"Venus"	  : 4.867 * pow(10, 24),
+	"Earth"	  : 5.962 * pow(10, 24),
+	"Mars"	  : 6.390 * pow(10, 23),
+	"Jupiter" : 1.898 * pow(10, 27),
+	"Saturn"  : 5.683 * pow(10, 26),
+	"Uranus"  : 8.681 * pow(10, 25),
+	"Neptune" : 1.024 * pow(10, 26)
+}
+	
 #The Schwarzschild Radius formula
 def schwarzschild(gravity, mass, light):
 	r = (2 * gravity * mass) / pow(light, 2)
@@ -13,6 +26,13 @@ def volume(pi, radius):
 	volume = (4/3) * pi * pow(radius, 3)
 	return volume
 
-radius = schwarzschild(gravity, mass, c)
+#Calculates schwarzschild radius for Earth
+#radius = schwarzschild(gravity, mass, c)
 
-print("Radius: " + str(radius * pow(10, 3)) + "mm" +"\nDiameter: " + str(radius * pow(10, 2) * 2) + "cm" + "\nVolume: " + str(volume(pi, radius * pow(10, 2))) + "cm3")
+for planet in masses:
+    print(planet)
+    radius = schwarzschild(gravity, masses[planet], c)
+    print("Radius: " + str(radius * pow(10, 3)) + "mm")
+    print("Diameter: " + str(radius * pow(10, 2) * 2) + "cm")
+    print("Volume: " + str(volume(pi, radius * pow(10, 2))) + "cm3")
+    print(" ")
